0ba7c05a12d0df2dcd6b992076cdd3316eb11967
R4ven <160066890+1nOnlyMrPixel@users.noreply.github.com>
2024-03-26 02:28:11 +0530

    ADD IMAGE AND NEW GUI UI
    Added GUI and Images and refactored the code.

--- script.js
+++ script.js
@@ -1,123 +1,171 @@
-function getComputerChoice()
-{
-let cmove=Math.floor(Math.random()*3)+1;
-if(cmove===1)
-    return "Rock";
-if(cmove===2)
-    return "Paper";
-if(cmove===3)
-    return "Scissor"
+let usrmove = "";
+let usr_score = 0,
+  pc_score = 0;
+
+const usr = document.querySelector(".us_ch_box");
+const compmoveGuirock = document.querySelector("#pc_rock");
+const compmoveGuipaper = document.querySelector("#pc_paper");
+const compmoveGuiscissor = document.querySelector("#pc_scissor");
+const resetBtn = document.querySelector("#resetBtn");
+usr.addEventListener("click", usrMove);
+usr.addEventListener("mouseout", resetpcmove);
+resetBtn.addEventListener("click", gameDefault);
+
+function gameDefault() {
+  resetScore();
+  defaultTitle();
+  contentVisibility(document.querySelector(".pc_ch_box"), "flex");
+  contentVisibility(document.querySelector(".us_ch_box"), "flex");
+  contentVisibility(document.querySelector(".scorebox"), "flex");
+  contentVisibility(resetBtn, "none");
 }
-//console.log(getComputerChoice());
-function gameRound(playerSelection,computerSelection)
-{
-    const ps=playerSelection.toUpperCase();
-    const cs=computerSelection.toUpperCase();
-console.log(`You Selected:${playerSelection}\nComputer Selected:${computerSelection}`);
-
-    if(ps===cs)
-    return ("t");
-
-        //v2
-
-
-    if(ps==="ROCK" && cs==="SCISSOR")
-        return ("w");
-    if(ps==="PAPER" && cs==="ROCK")
-        return ("w");
-    if(ps==="SCISSOR" && cs==="PAPER")
-        return ("w");
-
-
-
-    //ROCK PAPER PC
-    if(ps==="ROCK" && cs==="PAPER")
-      return("l");
-    if(ps==="PAPER" && cs==="SCISSOR")
-        return("l");
-    if(ps==="SCISSOR" && cs==="ROCK")
-        return("l");
-
-    // ROCK PAPER PLAYER
-    // if(ps==="ROCK" && cs==="SCISSOR")
-    //     return (`You Won ! ${ps} beats ${cs}`);
-    // if(ps==="PAPER" && cs==="ROCK")
-    //     return (`You Won ! ${ps} beats ${cs}`);
-    // if(ps==="SCISSOR" && cs==="PAPER")
-    //     return (`You Won ! ${ps} beats ${cs}`);
-
-
-
-    // ROCK PAPER PC
-    // if(ps==="ROCK" && cs==="PAPER")
-    //   return(`You Lose! ${computerSelection} beats ${playerSelection}`);
-    // if(ps==="PAPER" && cs==="SCISSOR")
-    //     return(`You Lose! ${computerSelection} beats ${playerSelection}`);
-    // if(ps==="SCISSOR" && cs==="ROCK")
-    //     return(`You Lose! ${computerSelection} beats ${playerSelection}`);
-
-
-
-
-    
-}   
-function playGame()
-{   
-    let us,pc,t;
-    us=pc=t=0;
-    let r=" ";
-    
-    
-    console.log("Round #1");
-
-    r=gameRound(prompt("Enter your choice !!"),getComputerChoice());
-    (r==="w")?us+=1:((r==="l")?pc+=1:t+=1);
-    console.log(r);
-    console.log("Round #2");
-    
-    r=gameRound(prompt("Enter your choice !!"),getComputerChoice());
-    (r==="w")?us+=1:((r==="l")?pc+=1:t+=1);
-    console.log(r);
-    
-    
-    console.log("Round #3");
-    
-    r=gameRound(prompt("Enter your choice !!"),getComputerChoice());
-    (r==="w")?us+=1:((r==="l")?pc+=1:t+=1);
-    console.log(r);
-    
-    
-    console.log("Round #4");
-    
-    r=gameRound(prompt("Enter your choice !!"),getComputerChoice());
-    (r==="w")?us+=1:((r==="l")?pc+=1:t+=1);
-    console.log(r);
-    
-    
-    
-    console.log("Round #5");
-    
-    r=gameRound(prompt("Enter your choice !!"),getComputerChoice());
-    (r==="w")?us+=1:((r==="l")?pc+=1:t+=1);
-    console.log(r);
-
-
-    if(us>pc)
-    {
-    console.log(`You are the Winner \(:\nYour Score=${us}\nComputer\'s Score=${pc}`);
-    }
-    else if(pc>us){
-    console.log(`You are the Loser \): \nComputer\'s Score=${pc}\nYour Score=${us}`);
-    }
-    else if(pc===us){
-         console.log(`Its a tie \): \nComputer\'s Score=${pc}\nYour Score=${us}`);
-    }
-    else
-        console.log(`Some Values improper cant determine`);
+function resetpcmove(e) {
+  let tar2 = e.target.id;
+  switch (tar2) {
+    case "rockimg":
+    case "us_rock":
+    case "paperimg":
+    case "us_paper":
+    case "scissorimg":
+    case "us_scissor":
+      resetColor(compmoveGuirock);
+      resetColor(compmoveGuipaper);
+      resetColor(compmoveGuiscissor);
+      break;
+  }
+  //resets title to default
+  if (usr_score < 5 && pc_score < 5) {
+    defaultTitle();
+  }
 }
-playGame();
+function resetColor(elementSel) {
+  elementSel.setAttribute(
+    "style",
+    "border-radius: 20px;border: 2px solid black;background-color:white"
+  );
+}
 
+function computerMove() {
+  let compmoveinnumb = Math.floor(Math.random() * 3 + 1);
+  resetColor(compmoveGuirock);
+  resetColor(compmoveGuipaper);
+  resetColor(compmoveGuiscissor);
 
+  if (compmoveinnumb == 1) {
+    compmoveGuirock.setAttribute(
+      "style",
+      "background-color: rgb(107, 165, 255);border: 2px solid rgb(107, 165, 255)"
+    );
+    return "rock";
+  }
+  if (compmoveinnumb == 2) {
+    compmoveGuipaper.setAttribute(
+      "style",
+      "background-color: rgb(107, 165, 255);border: 2px solid rgb(107, 165, 255)"
+    );
+    return "paper";
+  }
+  if (compmoveinnumb == 3) {
+    compmoveGuiscissor.setAttribute(
+      "style",
+      "background-color: rgb(107, 165, 255);border: 2px solid rgb(107, 165, 255)"
+    );
+    return "scissor";
+  }
+}
+function usrMove(e) {
+  resetpcmove(e);
+  usrmove = "";
+  let roundWinner = "";
+  let tar = e.target.id;
+  if (tar !== "") {
+    let compmove = computerMove();
+    switch (tar) {
+      case "rockimg":
+      case "us_rock":
+        usrmove = "rock";
+        break;
+      case "paperimg":
+      case "us_paper":
+        usrmove = "paper";
+        break;
+      case "scissorimg":
+      case "us_scissor":
+        usrmove = "scissor";
+        break;
+    }
+    roundWinner = checkRoundWinner(compmove);
+    if (roundWinner == "user") {
+      usr_score += 1;
+      changeTitle(document.querySelector(".titleUser"), "You Won!!");
+    }
+    if (roundWinner == "pc") {
+      pc_score += 1;
+      changeTitle(document.querySelector(".titleComputer"), "Computer Won!!");
+    }
+    if (roundWinner == "tie") {
+      changeTitle(document.querySelector(".titleUser"), "Tie!!");
+      changeTitle(document.querySelector(".titleComputer"), "Tie!!");
+    }
+    updateScore();
+    if (pc_score >= 5) {
+      changeTitle(
+        document.querySelector(".titleComputer"),
+        "Computer Winner!!"
+      );
+      changeTitle(document.querySelector(".titleUser"), `Score:${pc_score}`);
+      contentVisibility(document.querySelector(".pc_ch_box"), "none");
+      contentVisibility(document.querySelector(".us_ch_box"), "none");
+      contentVisibility(document.querySelector(".scorebox"), "none");
+      displayResetBtn();
+    }
+    if (usr_score >= 5) {
+      changeTitle(
+        document.querySelector(".titleComputer"),
+        "You are the Winner!!"
+      );
+      changeTitle(document.querySelector(".titleUser"), `Score:${usr_score}`);
+      contentVisibility(document.querySelector(".pc_ch_box"), "none");
+      contentVisibility(document.querySelector(".us_ch_box"), "none");
+      contentVisibility(document.querySelector(".scorebox"), "none");
+      displayResetBtn();
+    }
+  }
+}
+function checkRoundWinner(compmove) {
+  //USER WINNER
+  if (compmove === "rock" && usrmove === "paper") return "user";
+  if (compmove === "paper" && usrmove === "scissor") return "user";
+  if (compmove === "scissor" && usrmove === "rock") return "user";
 
-
-
+  //PC WINNER
+  if (compmove === "rock" && usrmove === "scissor") return "pc";
+  if (compmove === "paper" && usrmove === "rock") return "pc";
+  if (compmove === "scissor" && usrmove === "paper") return "pc";
+  //TIE
+  if (compmove === usrmove) return "tie";
+}
+function changeTitle(elemnt, txt) {
+  elemnt.textContent = txt;
+}
+function resetScore() {
+  usr_score = 0;
+  pc_score = 0;
+  updateScore();
+}
+function updateScore() {
+  let usc = document.querySelector("#us_score");
+  let psc = document.querySelector("#pc_score");
+  usc.textContent = usr_score;
+  psc.textContent = pc_score;
+}
+function contentVisibility(Winnerelement, value) {
+  Winnerelement.setAttribute("style", `display:${value}`);
+}
+function displayResetBtn() {
+  resetBtn.setAttribute("style", "display:inline");
+}
+function defaultTitle() {
+  changeTitle(document.querySelector(".titleUser"), "User");
+  changeTitle(document.querySelector(".titleComputer"), "Computer");
+}
